4cf3e0ae0792c4118eb3041525fe899ab555daa4
Tuan Vu <Nguyentuanvu1410@gmail.com>
2025-05-27 01:50:15 +0700

    fix LOCK

---
+++ src/main/java/com/example/be/constants/AppConstants.java
@@ -0,0 +1,7 @@
+package com.example.be.constants;
+
+public class AppConstants {
+//    public static final String Client_URL = "http://localhost:5173";
+    public static final String Client_URL = "https://shiny-vacherin-d266c6.netlify.app";
+
+}
