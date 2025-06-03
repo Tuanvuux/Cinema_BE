726f43b2ac2e22dff88596b1f6ef2cbc3a3847ec
Vanthuyen <125105464+Vanthuyen@users.noreply.github.com>
2025-06-03 09:29:02 +0700

    thuyen/update Post and testbug

--- src/main/java/com/example/be/constants/AppConstants.java
+++ src/main/java/com/example/be/constants/AppConstants.java
@@ -1,7 +1,6 @@
 package com.example.be.constants;
 
 public class AppConstants {
-    public static final String Client_URL = "http://localhost:5173";
-//    public static final String Client_URL = "https://cinex-cinema.netlify.app";
-
+//    public static final String Client_URL = "http://localhost:5173";
+    public static final String Client_URL = "https://cinex-cinema.netlify.app";
 }
