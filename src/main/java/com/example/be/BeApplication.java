8fe669caee11c323016a22283d223178d887e7ff
Vanthuyen <125105464+Vanthuyen@users.noreply.github.com>
2025-07-05 09:32:47 +0700

    thuyen/update link FE

--- src/main/java/com/example/be/BeApplication.java
+++ src/main/java/com/example/be/BeApplication.java
@@ -1,10 +1,14 @@
 package com.example.be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.example.be.repository")
+@EntityScan(basePackages = "com.example.be.entity")
 public class BeApplication {
     public static void main(String[] args) {
         SpringApplication.run(BeApplication.class, args);
--- src/main/java/com/example/be/config/SecurityConfig.java
+++ src/main/java/com/example/be/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://cinex-cinema.netlify.app"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://cinema-cinex.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
